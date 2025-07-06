acfe0d55a416def4d5bffe05a16f9d1c2c892617
kesavbgp <venkat.kesav@tihiitb.org>
2025-07-06 14:26:26 +0530

    New Patram 2 training paramaters

--- molmo_sft.sh
+++ molmo_sft.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=defq
-#SBATCH --nodelist=bharatgpt158
+#SBATCH --nodelist=bharatgpt010,bharatgpt012,bharatgpt155,bharatgpt157,bharatgpt158
 #SBATCH --job-name=grvqa_swift_training
 #SBATCH --mem-per-cpu=4G
 #SBATCH --cpus-per-task=64
@@ -15,14 +15,13 @@
 export USERNAME="venkat_kesav"
 export MODELSCOPE_CACHE="/workspace/models" 
 
-
 nodes=( $( scontrol show hostnames $SLURM_JOB_NODELIST ) )
 nodes_array=($nodes)
 head_node=${nodes_array[0]}
 head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" bash -c "hostname -I | tr ' ' '\n' | grep '^10\.' | head -n 1")
 head_node_port=$((10000 + SLURM_JOBID%10000))
 
-srun --nodes=${SLURM_NNODES} --ntasks=${SLURM_NNODES} sg docker -c 'hostname && docker load -i ./swift.tar'
+srun --nodes=${SLURM_NNODES} --ntasks=${SLURM_NNODES} sg docker -c 'hostname && docker load -i ./swift_vis_grp.tar'
 
 export LOGLEVEL=INFO
 export OMP_NUM_THREADS=1
@@ -33,10 +32,8 @@ export WORLD_SIZE=$(($SLURM_NNODES * 8))
 export MASTER_ADDR=${head_node_ip}
 export MASTER_PORT=${head_node_port}
 
-
 # Creating a folder for storing checkpoints, job-wise.
 srun --nodes=1 --ntasks=1 -w "$head_node" bash -c "mkdir output_${SLURM_JOBID}"
-
 
 # Kill the dangling containers of previous job if they still exist
 srun --nodes=${SLURM_NNODES} --ntasks=${SLURM_NNODES} sg docker -c 'docker rm -f kesav_ddp_train_swift || true'
@@ -55,7 +52,7 @@ srun --nodes=${SLURM_NNODES} --ntasks=${SLURM_NNODES} sg docker -c 'docker run -
 -v /projects/data/vision-team/venkat_kesav/GR_Model_Training_with_Swift:/workspace \
 -v /projects/data/vision-team:/projects/data/vision-team \
 -v /projects/data/vision-team/venkat_kesav/GR_Model_Training_with_Swift/output_${SLURM_JOBID}:/workspace/output \
-swift_img:tr_and_wb_fix bash -c "source /home/venkat_kesav/.venv/bin/activate && \
+swift:vis_grp bash -c "source /home/venkat_kesav/.venv/bin/activate && \
 torchrun \
     --master_port ${MASTER_PORT} \
     --nproc_per_node=8 \
@@ -63,9 +60,9 @@ torchrun \
     --master_addr=${MASTER_ADDR} \
     --node_rank=${SLURM_NODEID} \
     /home/venkat_kesav/ms-swift/swift/cli/sft.py \
-    --model allenai/Molmo-7B-O-0924 \
+    --model /projects/data/vision-team/venkat_kesav/GR_Model_Training_with_Swift/final_outputs/output_5190/v0-20250526-050001/checkpoint-12440 \
     --train_type full \
-    --dataset /projects/data/vision-team/hrithik_sagar/synthetic_qa_outputs/swift_training_format_3#10 \
+    --dataset /projects/data/vision-team/hrithik_sagar/synthetic_qa_outputs/swift_training_format_7 \
     --dataset_num_proc 16 \
     --use_hf True \
     --dataloader_num_workers 16 \
@@ -86,7 +83,8 @@ torchrun \
     --report_to wandb \
     --model_author ModelScope \
     --model_name molmo-docvqa \
-    --deepspeed zero2"'
-
+    --deepspeed zero2"'    
 
 # Removed the option --save_total_limit ( default is None, but can specify how much to save)
+#     --resume_from_checkpoint /projects/data/vision-team/venkat_kesav/GR_Model_Training_with_Swift/output_5115/v0-20250525-063349/checkpoint-6200 \
+#     --model allenai/Molmo-7B-O-0924 \
